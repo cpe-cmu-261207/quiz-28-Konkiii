6f967c43f0956c215f74a4674504e973761f1738
Konkiii <lovegangajaa99@gmail.com>
2022-08-04 14:14:25 +0700

    ...

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const btnRandom = document.querySelector("#btn-random");
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
 }
+
+btnRandom.onclick = async () => {
+  btnRandom.innerText = "";
+};
